74e8c96e44f2b2391ccbe9d8b1302eeb65bce6d9
xcheche <chekwubechukwu23@gmail.com>
2024-09-11 08:10:33 +0100

    search functionality done

--- dummy/views.py
+++ dummy/views.py
@@ -7,8 +7,21 @@ from django.core.paginator import Paginator
 
 def index(request):
     movies = Movies.objects.all()
+    
+    #Search logic
+    movie_name = request.GET.get('movie_name')
+  
+ 
+    if movie_name != '' and movie_name is not None:
+        movies= movies.filter(name__icontains=movie_name)
+        
+  
+    # End of search logic
+    
+    #Paginator
     pagiantor = Paginator(movies, 3)
     page = request.GET.get("page")
     movies = pagiantor.get_page(page)
+    # End of paginator
     context = {"movies": movies}
     return render(request, "dummy/index.html", context)
